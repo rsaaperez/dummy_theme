f2252b2c8a650b76b365e718974f6fc39d3687fa
Raúl <rsaaperez@danielcastelao.org>
2015-04-01 17:40:48 +0200

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,21 @@
+{
+    # Theme information
+    'name': "Dummy Theme",
+    'description': """
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates, pages, and snippets
+    'data': [
+        'views/pages.xml', 
+	#   'views/options.xml',
+    #    'views/snippets.xml',
+    ],
+
+    # Your information
+    'author': "Your Company",
+    'website': "",
+	
+}
